a41e54ffb7f296cb21d4bb86abb90c628ca9a62c
Abdul Wadood <abdulwd97@gmail.com>
2018-11-23 22:08:26 +0530

    Use Rx for delay

--- app/src/main/java/com/abdulwd/talk/MainActivity.kt
+++ app/src/main/java/com/abdulwd/talk/MainActivity.kt
@@ -9,17 +9,22 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
-import android.preference.PreferenceManager
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import net.gotev.speech.Speech
 import net.gotev.speech.SpeechDelegate
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : Activity() {
 
@@ -27,48 +32,15 @@ class MainActivity : Activity() {
     private var inStream: InputStream? = null
     private val requestBluetoothEnable = 1
     private val requestAudioPermission = 2
+    private val tag = "AbdulWadood"
     private val words = LinkedList<String>()
     private val displayText = StringBuilder()
-    private val displayMaxHeight = 2
-    private val displayMaxWidth = 10
+    private val displayMaxHeight = 4
+    private val displayMaxWidth = 21
+    private val delay = 1000L
+    private val stopListening = "Stop Listening"
     private val displaySize = displayMaxHeight * displayMaxWidth
     private val defaultUUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb")
-    private val handler = Handler()
-    private var handlerCompleted = true
-    private val runnable: Runnable = object : Runnable {
-        override fun run() {
-            runOnUiThread {
-                Log.d("Abdul", "run")
-                if (words.isEmpty()) {
-                    inputEdiText.setText("")
-                    sendText()
-                    return@runOnUiThread
-                }
-                if (words.peek().length + displayText.length > displaySize) {
-                    displayText.setLength(0)
-                }
-
-                while (words.peek() != null &&
-                    displayText.length + words.peek().length <= displaySize
-                ) {
-                    displayText.append(words.poll())
-                    displayText.append(' ')
-                }
-                inputEdiText.setText(displayText)
-                sendText.callOnClick()
-            }
-            if (words.isNotEmpty()) {
-                handler.postDelayed(
-                    this, 1000L *
-                            PreferenceManager
-                                .getDefaultSharedPreferences(this@MainActivity)
-                                .getInt("delay", 5)
-                )
-            } else {
-                handlerCompleted = true
-            }
-        }
-    }
     private val speechDelegate: SpeechDelegate = object : SpeechDelegate {
         override fun onStartOfSpeech() {
 
@@ -83,14 +55,56 @@ class MainActivity : Activity() {
         }
 
         override fun onSpeechResult(result: String?) {
-            result?.let { it.split(" ").forEach { word -> words.add(word) } }
-            if (handlerCompleted) {
-                handlerCompleted = false
-                handler.post(runnable)
+            result?.let {
+                it.split(" ").forEach { word ->
+                    if (word.isNotEmpty())
+                        words.add(word)
+                }
+                addToTextView(it)
             }
-            Handler().post { startVoiceRecognition() }
+
+            if (result != null && result.isNotEmpty() &&
+                (disposable == null || disposable?.isDisposed == true)
+            ) {
+                Observable
+                    .interval(0, delay, TimeUnit.MILLISECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(object : Observer<Long> {
+                        override fun onComplete() {
+                            Log.d(tag, "on Complete")
+                        }
+
+                        override fun onSubscribe(d: Disposable) {
+                            disposable = d
+                        }
+
+                        override fun onNext(l: Long) {
+                            if (words.isEmpty()) {
+                                return
+                            }
+                            if (displayText.length + words.peek().length > displaySize) {
+                                displayText.setLength(0)
+                            }
+                            displayText.append(words.poll())
+                            displayText.append(' ')
+                            Log.d(tag, displayText.toString())
+                            write(displayText.toString())
+                        }
+
+                        override fun onError(e: Throwable) {
+
+                        }
+                    })
+            }
+
+            if (flag && startButton.text.toString().equals(stopListening, true)) {
+                Handler().post { startVoiceRecognition() }
+            }
         }
     }
+    private var flag = false
+    private var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -98,13 +112,22 @@ class MainActivity : Activity() {
 
         connectBluetoothDevice()
         Speech.init(this, packageName)
-        sendText.setOnClickListener { sendText() }
-        speechProgressView.setOnClickListener { startVoiceRecognition() }
+        sendText.setOnClickListener {
+            inputEdiText?.editableText?.toString()?.let { it1 ->
+                addToTextView(it1)
+                write(it1)
+            }
+        }
+        startButton.setOnClickListener {
+            if (getString(R.string.start_listening).equals(startButton.text.toString(), true)) {
+                startVoiceRecognition()
+            } else {
+                stopVoiceRecognition()
+            }
+        }
     }
 
-    fun sendText() {
-        val inputText = inputEdiText.editableText
-        write(inputText.toString())
+    fun addToTextView(inputText: CharSequence) {
         if (inputText.isNotEmpty()) {
             inputEdiText.setText("")
             val output = if (outputTextView.text != null)
@@ -128,9 +151,7 @@ class MainActivity : Activity() {
         permissions: Array<String>,
         grantResults: IntArray
     ) {
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            startVoiceRecognition()
-        } else {
+        if (!grantResults.isNotEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Audio permissions are required", Toast.LENGTH_LONG).show()
         }
     }
@@ -171,8 +192,6 @@ class MainActivity : Activity() {
                 arrayOf(Manifest.permission.RECORD_AUDIO),
                 requestAudioPermission
             )
-        } else {
-            startVoiceRecognition()
         }
     }
 
@@ -190,12 +209,19 @@ class MainActivity : Activity() {
     }
 
     private fun startVoiceRecognition() {
+        flag = true
         Speech.getInstance().startListening(speechProgressView, speechDelegate)
+        startButton.text = stopListening
+    }
+
+    private fun stopVoiceRecognition() {
+        flag = false
+        Speech.getInstance().stopListening()
+        startButton.text = getString(R.string.start_listening)
     }
 
     override fun onDestroy() {
-        handler.removeCallbacks(runnable)
-        Speech.getInstance().stopTextToSpeech()
+        stopVoiceRecognition()
         super.onDestroy()
     }
 }
